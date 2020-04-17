160019148202b23f5d10f778487ec154d2189430
Yashesh Trivedi <yasheshtrivedi1@gmail.com>
2020-04-17 19:56:18 +0530

    Cycle now moves on clicking next or previous btn

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let nextButton = document.getElementById("next-btn");
 let prevButton = document.getElementById("prev-btn");
 let wheels = document.querySelectorAll(".wheel");
+let cycleContainer = document.querySelector(".cycle-container");
 
 
 ///// Function to add and remove class from a list of elements(nodelist)
@@ -21,23 +22,31 @@ let removeClassFromNodeList = function(nodeList, className) {
 };
 
 nextButton.addEventListener('click', () => {
-    /* removing the move-forward class from each element of nodelist,
-       in case where prev button is clicked and after that instantly 
-       next button is clicked, before the timeout callback is called. */
+    /* remove any undeleted classes, added by previous button's onclick callback. */
     removeClassFromNodeList(wheels, "move-forward");
     // Add class which moves the cycle wheels backward.
     addClassToNodeList(wheels, "move-backward");
+
+    cycleContainer.classList.add("move-container-left");
+
     // Clear the class after first iteration of the animation ends
-    setTimeout(() => removeClassFromNodeList(wheels, "move-backward"), 3500);
+    setTimeout(() => {
+        removeClassFromNodeList(wheels, "move-backward");
+        cycleContainer.classList.remove("move-container-left");
+    }, 3500);
 });
 
 prevButton.addEventListener('click', () => {
-    /* removing the move-backward class from each element of the nodelist,
-       in case where next button is clicked and after that instantly 
-       previous button is clicked, before the timeout callback was called. */
+    /* remove any undeleted classes, added by next button's onclick callback. */
    removeClassFromNodeList(wheels, "move-backward");
    // Add class which moves the cycle wheels forward.
    addClassToNodeList(wheels, "move-forward");
+
+   cycleContainer.classList.add("move-container-right");
+
    // Clear the class after first iteration of the animation ends
-   setTimeout(() => removeClassFromNodeList(wheels, "move-forward"), 3500);
+   setTimeout(() => {
+    removeClassFromNodeList(wheels, "move-forward");
+    cycleContainer.classList.remove("move-container-right");
+}, 3500);
 });
