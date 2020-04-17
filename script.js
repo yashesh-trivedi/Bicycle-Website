9d4f0d20d588db95144f517335af342de9d862fd
Yashesh Trivedi <yasheshtrivedi1@gmail.com>
2020-04-17 17:00:03 +0530

    cleaned up code and fixed the variables of next and prev button

--- script.js
+++ script.js
@@ -1,25 +1,8 @@
-let nextButton = document.getElementById("prev-btn");
-let prevButton = document.getElementById("next-btn");
-
-console.log(nextButton);
-
+let nextButton = document.getElementById("next-btn");
+let prevButton = document.getElementById("prev-btn");
 let frontWheels = document.querySelectorAll(".front-wheel");
-let backWheels = document.querySelectorAll(".back-wheel");
+let backWheels = document.querySelectorAll(".back-wheel"); 
 
-console.log(frontWheels);
-
-// nextButton.onclick = function moveToNextCycle() {
-//     frontWheels.forEach(element => {
-//         console.log(element);
-//         element.style = "animation: 5s rotate-wheel linear infinite";
-//     });
-
-//     backWheels.forEach(element => {
-//         console.log(element);
-//         element.style = "animation: 5s rotate-wheel linear infinite";
-//     });
-
-// };  
 
 nextButton.addEventListener('click',() => {
     for( let i = 0 ; i<frontWheels.length ; i++){
